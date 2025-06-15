9759efc579ab3904148385bf5624ec4bb1a5e4d7
IT Department <it@mjm-bali.co.id>
2025-06-15 15:43:16 +0800

    fix fayroll

--- app/Imports/PayrollsImport.php
+++ app/Imports/PayrollsImport.php
@@ -105,8 +105,8 @@ $bpjs_kes            = isset($row[10]) ? (float) $row[10] : 0.0;
 $bpjs_ket            = isset($row[11]) ? (float) $row[11] : 0.0;
 $tax                 = isset($row[12]) ? (float) $row[12] : 0.0;
 $debt                 = isset($row[13]) ? (float) $row[13] : 0.0;
-$deductions = $late_fine + $punishment + $bpjs_kes + $bpjs_ket + $tax + $debt;
-
+$deductions = $late_fine + $punishment + ($bpjs_kes * 2)   
+            + ($bpjs_ket * 2) + $tax + $debt;
 $salary = ($attendance * $daily_allowance)
         + $house_allowance
         + $meal_allowance
