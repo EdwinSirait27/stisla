17d36d3ea8bd89bb20069710add73f5f2e2d859d
Edwin Sirait <drummerboy794@gmail.com>
2026-04-24 14:13:58 +0800

    lupa force https

--- app/Models/Fingerprintrecap.php
+++ app/Models/Fingerprintrecap.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
-class FingerprintRecap extends Model
+class Fingerprintrecap extends Model
 {
     use HasUuids;
     protected $table = 'fingerprints_recap';
