c89d6425973802699b55fbf7136eebbb01a93f64
Edwin Sirait <drummerboy794@gmail.com>
2026-07-08 09:03:41 +0800

    perbaikan payroll dengan shouldqueue v2 tadi ada error

--- app/Mail/PayrollSlipMail.php
+++ app/Mail/PayrollSlipMail.php
@@ -35,6 +35,36 @@ use Illuminate\Queue\SerializesModels;
 //             ]);
 //     }
 // }
+// class PayrollSlipMail extends Mailable
+// {
+//     use Queueable, SerializesModels;
+
+//     public function __construct(
+//         public Payroll $payroll,
+//         public string $pdfPath,
+//         public string $slipPassword
+//     ) {}
+
+//     public function build()
+//     {
+//         $period = \Carbon\Carbon::parse($this->payroll->period_start)->translatedFormat('F Y');
+
+//         return $this->subject('Slip Gaji - ' . $period . ' - ' . $this->payroll->employee->employee_name)
+//             ->view('mail.PayrollSlipMail')
+//             ->with([
+//                 'employeeName'   => $this->payroll->employee->employee_name,
+//                 'payrollPeriod'  => $period,
+//                 'netSalary'      => $this->payroll->net_salary,
+//                 'totalDeduction' => $this->payroll->total_deduction,
+//                 'companyName'    => $this->payroll->employee->company->name ?? 'PT. Asian Bay Development',
+//                 'slipPassword'   => $this->slipPassword,
+//             ])
+//             ->attach($this->pdfPath, [
+//                 'as'   => 'Slip_Gaji_' . $this->payroll->employee->employee_pengenal . '.pdf',
+//                 'mime' => 'application/pdf',
+//             ]);
+//     }
+// }
 class PayrollSlipMail extends Mailable
 {
     use Queueable, SerializesModels;
